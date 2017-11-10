8ae9565c43ff8b1c43f697ee28dc8f3f9fffaf7f
boris.hernandez <boris.hceron@gmail.com>
2017-11-10 17:51:28 -0600

    Adding reducers and actions and also views

---
+++ src/components/posts_new.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class postsNew extends Component {
+
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-control ${touched && error ? 'is-invalid' : ''}`
+
+        return (
+            <div className="form-group is-invalid">
+                <label className="form-control-label">{field.label}</label>
+                <input className={className} type="text" {...field.input} />
+                <div className="text-danger">
+                    { touched ? error : '' }
+                </div>
+            </div>
+        );
+    }
+
+    onSubmit(values) {
+        this.props.createPost(values, () => {
+            this.props.history.push("/");            
+        });
+        
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField} />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField} />
+                <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField} />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
+        );
+    }
+}
+
+function validateForm(values) {
+    const errors = {};
+    
+    if(!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters!";
+    }
+    if(!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+    if(!values.content) {
+        errors.content = "Enter some contenct please";
+    }
+
+
+    return errors;
+}
+
+export default reduxForm({
+    validate: validateForm,
+    form: 'PostNewForm'
+})(
+    connect(null, { createPost })(postsNew)
+);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware  } from 'redux';
+import promise from 'redux-promise';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
-import App from './App';
+import reducers from './reducers';
+import PostIndex from './components/post_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={ createStoreWithMiddleware(reducers) } >
+        <BrowserRouter>
+            <div className="container">
+                <Switch>
+                    <Route path='/posts/new' exact component={PostsNew} />
+                    <Route path='/posts/:id' component={PostsShow} />
+                    <Route path='/' component={PostIndex} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
